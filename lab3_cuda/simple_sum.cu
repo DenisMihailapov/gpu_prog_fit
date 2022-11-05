1a7fc0acafcfed1d1c9b813aace1168f6e884a23
DenisMihailapov <denismihailapov@yandex.ru>
2022-11-05 19:14:31 +0700

    2d array sum cuda. (use grids and blocks)

--- lab3_cuda/simple_sum.cu
+++ lab3_cuda/simple_sum.cu
@@ -1,5 +1,4 @@
 
-#include <cstdlib>
 #include <cuda_runtime.h>
 #include <stdio.h>
 
@@ -35,54 +34,93 @@ void checkLast(const char* const file, const int line)
 
 
 #define REAL float
-#define uint unsigned int 
 
-__global__ void add(REAL* a, REAL* b, REAL* c, uint N){
+int __host__ __device__ ind2D(const size_t i, const size_t j, const size_t width)
+{
+    return i + j*width;
+}
+
+__global__ void add2D(REAL* a, REAL* b, REAL* c, size_t N){
 
 	//Calculate the data at the index
-	uint index = blockIdx.x *blockDim.x + threadIdx.x;
+	size_t i = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t j = blockIdx.y * blockDim.y + threadIdx.y;
 
-	if(index < N)
-		c[index] = a[index] + b[index];
+	if (i < N && j < N)
+	{   
+		size_t ind = ind2D(i, j, N);
+		c[ind] = a[ind] + b[ind];
+	}	
 
+}
+
+void init_full(size_t N, REAL *array, REAL left_top, REAL right_top, REAL left_bottom, REAL right_bottom){
+    
+    for(size_t i = 0; i < N; i++){
+        array[ind2D(i, 0, N)] = left_top + i*(right_top - left_top) / N;
+        array[ind2D(i, N - 1, N)] = left_bottom + i*(right_bottom - left_bottom) / N;
+
+        for(size_t j = 0; j < N; j++)
+            array[ind2D(i, j, N)] = 
+			    array[ind2D(i, 0, N)] + 
+				j*(array[ind2D(i, N - 1, N)] - array[ind2D(i, 0, N)]) / N;
+    }
+}
+
+void print2D(REAL *array, size_t row, size_t col){
+	for (size_t j = 0; j < col; j++) {
+        for (size_t i = 0; i < row; i++) {
+            printf("%.4f ", array[ind2D(i, j, row)]);
+        }
+        printf("\n");
+    }
 }
 
 int main(int argc, char *argv[]){
 
-	uint N = atof(argv[1]);
-	uint MEM_ARRAY_SIZE = N * sizeof(REAL);
+	size_t N = atof(argv[1]);
+	size_t FULL_MEM_SIZE = N * N * sizeof(REAL);
     
 	//0. Allocate and Initialize memory on the CPU
-    REAL *a = (REAL *) std::malloc(MEM_ARRAY_SIZE);
-	REAL *b = (REAL *) std::malloc(MEM_ARRAY_SIZE);
-	REAL *c = (REAL *) std::malloc(MEM_ARRAY_SIZE);
+    REAL *a = (REAL *)std::malloc(FULL_MEM_SIZE);
+	REAL *b = (REAL *)std::malloc(FULL_MEM_SIZE);
+	REAL *c = (REAL *)std::malloc(FULL_MEM_SIZE);
     
-	for (int i = 0; i< N; i++) {
-		a[i] = -i; b[i] = i * i;
-	}
+	init_full(N, a, 1, 9, 9, 18);
+	init_full(N, b, 0, 8, 8, 16);
+
 	 //1. Allocate memory on the GPU
 	REAL *dev_a, *dev_b, *dev_c;
-	CHECK_CUDA_ERROR(cudaMalloc(&dev_a, N * sizeof(REAL)));
-	CHECK_CUDA_ERROR(cudaMalloc(&dev_b, N * sizeof(REAL)));
-	CHECK_CUDA_ERROR(cudaMalloc(&dev_c, N * sizeof(REAL)));
+	CHECK_CUDA_ERROR(cudaMalloc(&dev_a, FULL_MEM_SIZE));
+	CHECK_CUDA_ERROR(cudaMalloc(&dev_b, FULL_MEM_SIZE));
+	CHECK_CUDA_ERROR(cudaMalloc(&dev_c, FULL_MEM_SIZE));
  
 	 //2. Copy the arrays ‘a’ and ‘b’ to the GPU
-	CHECK_CUDA_ERROR(cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice));
-	CHECK_CUDA_ERROR(cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice));
+	CHECK_CUDA_ERROR(cudaMemcpy(dev_a, a, FULL_MEM_SIZE, cudaMemcpyHostToDevice));
+	CHECK_CUDA_ERROR(cudaMemcpy(dev_b, b, FULL_MEM_SIZE, cudaMemcpyHostToDevice));
  
 	 //3. Execute the kernel
-	dim3 BS(N);
-	dim3 GS(ceil(N/(float)BS.x));
+	dim3 BS(N/4., N/4.); 
+	dim3 GS(ceil(N/(float)BS.x), ceil(N/(float)BS.x));
+	printf("threads: %d, block size: %d\n\n", BS.x, GS.x);
 
-	add<<<BS, GS>>>(dev_a,dev_b,dev_c, N);
+	add2D<<<GS, BS>>>(dev_a, dev_b, dev_c, N);
  
 	 //4. Copy the array ‘c’ from GPU to CPU
-	CHECK_CUDA_ERROR(cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost));
+	CHECK_CUDA_ERROR(cudaMemcpy(c,  dev_c, FULL_MEM_SIZE, cudaMemcpyDeviceToHost));
  
 	 //Display the result on the CPU
-	for (int i = 0; i<N; i++) {
-		printf("%.4f + %.4f = %.4f\n", a[i], b[i], c[i]);
-	}
+	print2D(a, N, N);
+	printf("\n");
+	printf("    | \n");
+	printf(" ---|---\n");
+	printf("    | \n");
+	printf("\n");
+	print2D(b, N, N);
+	printf("\n");
+	printf("   ||\n");
+	printf("\n");
+	print2D(c, N, N);
  
 	 //5. Release the memory allocated on the GPU. Purpose to avoid memory leaks
 	cudaFree(dev_a); std::free(a);
