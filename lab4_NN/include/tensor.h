d0a9abbd5435bc393a1b1f5a4c5bdd191f429e3d
DenisMihailapov <denismihailapov@yandex.ru>
2022-12-23 10:15:09 +0000

    improve tensor constructor

--- lab4_NN/include/tensor.h
+++ lab4_NN/include/tensor.h
@@ -1,27 +1,30 @@
-#include <cmath>
-#include <iostream>
 #include <algorithm>
-#include <memory>
+#include <cmath>
 #include <cstddef>
+#include <iostream>
+#include <memory>
 
 #include "shape.h"
 #include "tensor_kern.h"
 
-
-template <class Scalar = double> 
-class Tensor {
+template <class Scalar = double> class Tensor {
 public:
   /* Constructors */
   Tensor<Scalar>(bool gpu = false);
-  Tensor<Scalar>(size_t height, size_t width, bool gpu = false);
-  Tensor<Scalar>(Shape shape, bool gpu = false);  
-  Tensor<Scalar>(size_t height, size_t width, Scalar min, Scalar max,
-                 bool gpu = false);
+  void init(size_t height, size_t width, bool gpu = false, Scalar min = 0,
+            Scalar max = 1, bool rand_init = false);
+  void init(Shape shape, bool gpu = false, Scalar min = 0, Scalar max = 1,
+            bool rand_init = false);
+
+  Tensor<Scalar>(size_t height, size_t width, bool gpu = false, Scalar min = 0,
+                 Scalar max = 1, bool rand_init = false);
+  Tensor<Scalar>(Shape shape, bool gpu = false, Scalar min = 0, Scalar max = 1,
+                 bool rand_init = false);
 
   /* Getters */
   size_t getWidth() const;
   size_t getHeight() const;
-  //Shape shape = Shape(0, 0); //TODO
+  // Shape shape = Shape(0, 0); //TODO
 
   Scalar get(size_t i, size_t j) const;
   void set(size_t i, size_t j, Scalar el);
@@ -62,39 +65,6 @@ private:
   bool gpu;
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 template <class Scalar> Tensor<Scalar>::Tensor(bool gpu) {
   static_assert(std::is_same<Scalar, int>::value ||
                     std::is_same<Scalar, float>::value ||
@@ -106,7 +76,9 @@ template <class Scalar> Tensor<Scalar>::Tensor(bool gpu) {
 }
 
 template <class Scalar>
-Tensor<Scalar>::Tensor(size_t height, size_t width, bool gpu) {
+void Tensor<Scalar>::init(size_t height, size_t width, bool gpu, Scalar min,
+                          Scalar max, bool rand_init) {
+
   static_assert(std::is_same<Scalar, int>::value ||
                     std::is_same<Scalar, float>::value ||
                     std::is_same<Scalar, double>::value,
@@ -115,33 +87,36 @@ Tensor<Scalar>::Tensor(size_t height, size_t width, bool gpu) {
   this->gpu = gpu;
 
   this->array = new Scalar[height * width];
-  srand(time(NULL));
+
+  if (rand_init)
+    srand(time(NULL));
+
   for (size_t i = 0; i < height; i++) {
     for (size_t j = 0; j < width; j++) {
-      Scalar n = -1 + rand() / Scalar(RAND_MAX) * 2;
-      this->set(i, j, n);
+      if (rand_init)
+        this->set(i, j, min + (max - min) * (rand() / Scalar(RAND_MAX)));
+      else
+        this->set(i, j, 0.);
     }
   }
 }
 
 template <class Scalar>
-Tensor<Scalar>::Tensor(size_t height, size_t width, Scalar min, Scalar max,
-                       bool gpu) {
-  static_assert(std::is_same<Scalar, int>::value ||
-                    std::is_same<Scalar, float>::value ||
-                    std::is_same<Scalar, double>::value,
-                "Type not allowed. Use <int>, <float> or <double>.");
-  this->shape = Shape(height, width);
-  this->gpu = gpu;
+void Tensor<Scalar>::init(Shape shape, bool gpu, Scalar min, Scalar max,
+                          bool rand_init) {
+  this->init(shape.height, shape.width, gpu, min, max, rand_init);
+}
 
-  this->array = new Scalar[height * width];
-  srand(time(NULL));
-  for (size_t i = 0; i < height; i++) {
-    for (size_t j = 0; j < width; j++) {
-      Scalar n = min + (max - min) * (rand() / Scalar(RAND_MAX));
-      this->set(i, j, n);
-    }
-  }
+template <class Scalar>
+Tensor<Scalar>::Tensor(size_t height, size_t width, bool gpu, Scalar min,
+                       Scalar max, bool rand_init) {
+  this->init(height, width, gpu, min, max, rand_init);
+}
+
+template <class Scalar>
+Tensor<Scalar>::Tensor(Shape shape, bool gpu, Scalar min, Scalar max,
+                       bool rand_init) {
+  this->init(shape.height, shape.width, gpu, min, max, rand_init);
 }
 
 template <class Scalar> size_t Tensor<Scalar>::getHeight() const {
@@ -173,18 +148,21 @@ void Tensor<Scalar>::set(size_t i, size_t j, Scalar el) {
 
 template <class Scalar>
 Tensor<Scalar> Tensor<Scalar>::add(const Tensor<Scalar> &m) const {
-  if (m.shape.height != this->shape.height || m.shape.width != this->shape.width) {
+  if (m.shape.height != this->shape.height ||
+      m.shape.width != this->shape.width) {
     fprintf(stderr,
             "Can't add element wise a matrix of shape (%li, %li) with a matrix "
             "of shape (%li, %li).\n",
-            this->shape.height, this->shape.width, m.shape.height, m.shape.width);
+            this->shape.height, this->shape.width, m.shape.height,
+            m.shape.width);
     throw;
   }
 
   Tensor<Scalar> result(shape.height, shape.width, this->gpu);
   if (gpu) {
     Wrapper().add((Scalar *)&this->array[0], (Scalar *)&m.array[0],
-                  (Scalar *)&result.array[0], this->shape.width * this->shape.height);
+                  (Scalar *)&result.array[0],
+                  this->shape.width * this->shape.height);
   } else {
     for (size_t i = 0; i < this->shape.height; i++) {
       for (size_t j = 0; j < this->shape.width; j++) {
@@ -199,7 +177,9 @@ template <class Scalar> Tensor<Scalar> Tensor<Scalar>::add(Scalar m) const {
 
   if (gpu)
     std::cout << "add Scalar calc on CPU" << std::endl;
-  Tensor<Scalar> result(shape.height, shape.width, this->gpu, this->gpu);
+
+  Tensor<Scalar> result(shape, this->gpu);
+
   for (size_t i = 0; i < this->shape.height; i++) {
     for (size_t j = 0; j < this->shape.width; j++) {
       result.set(i, j, this->get(i, j) + m);
@@ -214,7 +194,8 @@ Tensor<Scalar> Tensor<Scalar>::dot(const Tensor<Scalar> &m) const {
     fprintf(stderr,
             "Can't multiply a matrix of shape (%li, %li) with a matrix of "
             "shape (%li, %li).\n",
-            this->shape.height, this->shape.width, m.shape.height, m.shape.width);
+            this->shape.height, this->shape.width, m.shape.height,
+            m.shape.width);
     throw;
   }
   Tensor<Scalar> result(this->shape.height, m.shape.width, this->gpu);
@@ -252,11 +233,13 @@ template <class Scalar> Tensor<Scalar> Tensor<Scalar>::dot(Scalar m) const {
 
 template <class Scalar>
 Tensor<Scalar> Tensor<Scalar>::multiply(const Tensor<Scalar> &m) const {
-  if (m.shape.height != this->shape.height || m.shape.width != this->shape.width) {
+  if (m.shape.height != this->shape.height ||
+      m.shape.width != this->shape.width) {
     fprintf(stderr,
             "Can't multiply element wise a matrix of shape (%li, %li) with a "
             "matrix of shape (%li, %li).\n",
-            this->shape.height, this->shape.width, m.shape.height, m.shape.width);
+            this->shape.height, this->shape.width, m.shape.height,
+            m.shape.width);
     throw;
   }
   Tensor<Scalar> result(shape.height, shape.width, this->gpu);
@@ -398,7 +381,8 @@ Tensor<Scalar> Tensor<Scalar>::operator%(const Tensor<Scalar> &m) const {
   return this->multiply(m);
 }
 
-template <class Scalar> bool Tensor<Scalar>::operator==(const Tensor<Scalar> &m) {
+template <class Scalar>
+bool Tensor<Scalar>::operator==(const Tensor<Scalar> &m) {
   return this->eq(m);
 }
 
